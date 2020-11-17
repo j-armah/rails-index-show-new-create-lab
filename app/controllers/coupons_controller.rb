1a1e7ec49661e90cad54628fea27096029459585
februar-y <jarmah241@gmail.com>
2020-11-17 11:44:11 -0500

    commit tree 19a5ae39c0389e605f0356360fca54dff13b1bd0

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,33 @@
+class CouponsController < ApplicationController
+    def index
+        @coupons = Coupon.all
+    end
+
+    def show
+        @coupon = Coupon.find(params[:id])
+    end
+
+    def new
+       # @coupon = Coupon.new
+    end
+
+    # Isn't this create better than one in solution/rspec?
+    # def create
+    #     @coupon = Coupon.create(
+    #         {
+    #             coupon_code: params[:coupon_code],
+    #             store: params[:store]
+    #         }
+    #     )
+    #     @coupon.save
+    #     redirect_to coupon_path(@coupon)
+    # end
+
+    def create 
+        @coupon = Coupon.new
+        @coupon[:coupon_code] = params[:coupon][:coupon_code]
+        @coupon[:store] = params[:coupon][:store]
+        @coupon.save
+        redirect_to coupon_path(@coupon)
+    end
+end
